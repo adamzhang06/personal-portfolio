44123eb2c04b914f1b21e0d60f9bda9711ee9a48
Adam Zhang <reachadamzhang@gmail.com>
2026-02-04 12:16:16 -0500

    setup for all sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Experience } from "@/sections/Experience";
+import { Contact } from "@/sections/Contact";
+
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen overflow-x-hidden">
+      <NavBar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Contact />
+      </main>
+    </div>
   );
 }
 
